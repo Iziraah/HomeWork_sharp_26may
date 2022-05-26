23152ee97e431a1ae575cff944781dd36ced2329
Mariia <>
2022-05-26 20:02:08 +0300

    решение задачи 27

---
+++ Tasks/task_27/27.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на
+//вход число и выдаёт сумму цифр в числе. Задачу решить без использования строк (string).
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите натуральное число: ");
+int number = int.Parse(Console.ReadLine());
+//int count = 0;
+int sum = 0;
+Console.WriteLine($"Сумма цифр в числе {number} =  ");
+while (number > 0)
+{
+    sum =sum + number % 10;
+    number = number / 10;
+    //count ++;
+}
+Console.WriteLine(sum);
+
