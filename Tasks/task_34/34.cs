d117a8e6c915ba183e1312f59e2f67e98d354554
Mariia <>
2022-05-26 20:19:30 +0300

    решение задачи 34

---
+++ Tasks/task_34/34.cs
@@ -0,0 +1,19 @@
+﻿// Задайте массив заполненный случайными
+//положительными трёхзначными числами. Напишите
+//программу, которая покажет количество чётных чисел в
+//массиве.
+
+int[] nums = new int[12];
+Random numRnd = new Random();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = numRnd.Next(100, 1000);
+    Console.Write(nums[i] + " ");
+}
+int numPositive = 0;
+for (int i=0; i<nums.Length ; i++)
+{
+    if (nums[i]%2==0) numPositive += 1;
+}
+Console.WriteLine();
+Console.WriteLine(numPositive);
