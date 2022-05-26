6588e4469e589be64643b8c40f8da35d59b0e2ee
Mariia <>
2022-05-26 19:46:35 +0300

    решение задачи 25

---
+++ Tasks/task_25/25.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход
+//два числа (A и B) и возводит число A в натуральную
+//степень B.
+
+Console.WriteLine("Введите натуральное число A: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите натуральное число B: ");
+int numberB = int.Parse(Console.ReadLine());
+int sqr = numberA;
+for (int i=2; i<=numberB; i++)
+{
+    sqr = sqr * numberA;
+    
+}
+Console.WriteLine($"{numberA} в степени {numberB} = {sqr}");
