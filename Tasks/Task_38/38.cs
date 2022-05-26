d8f307d2d2c6b6d5dec6abb0dec19f0ce38df8bb
Mariia <>
2022-05-26 20:47:18 +0300

    решение задачи 38

---
+++ Tasks/Task_38/38.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив вещественных чисел. Найдите
+//разницу между максимальным и минимальным
+//элементов массива.
+
+int[] nums = new int[12];
+Random numRnd = new Random();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = numRnd.Next(1, 100);
+    Console.Write(nums[i] + " ");
+}
+
+int GetMax = nums[0];
+int GetMin = nums[0];
+for (int i=0; i<nums.Length ; i++)
+{
+    if (nums[i] < GetMin)
+    {
+        GetMin = nums[i];
+    }
+    else
+    if (nums[i] > GetMax)
+    {
+        GetMax = nums[i];
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine($"Максимальное число - {GetMax}");
+Console.WriteLine($"Минимальное число - {GetMin}");
+int diff = GetMax - GetMin;
+Console.WriteLine($"Разница между максимальным и минимальным числом - {diff}");
